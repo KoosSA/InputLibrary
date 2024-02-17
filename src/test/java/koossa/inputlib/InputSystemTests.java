013347a99ff483d399bddd89a2bd8ad756e95b7a
Koos <kkposbus@gmail.com>
2024-02-17 19:42:09 +0200

    Updated unit tests to represent inaccuracies better (10% fps dif)

--- src/test/java/koossa/inputlib/InputSystemTests.java
+++ src/test/java/koossa/inputlib/InputSystemTests.java
@@ -2,57 +2,66 @@ package koossa.inputlib;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import com.koossa.filesystem.CommonFolders;
 import com.koossa.filesystem.Files;
 import com.koossa.filesystem.RootFileLocation;
 import com.koossa.logger.Log;
+import com.koossa.savelib.SaveSystem;
 
 class InputSystemTests {
+	
+	@BeforeAll
+	static void init() {
+		Files.init("Resources", RootFileLocation.LOCAL);
+		Log.init(Files.getCommonFolder(CommonFolders.Logs), true);
+		SaveSystem.init(Files.getCommonFolder(CommonFolders.Saves), Files.getFolder("Data"));
+		System.out.println("Starting tests");
+	}
+	
+	@AfterAll
+	static void dispose() {
+		Log.disposeAll();
+		System.out.println("Stopping tests");
+	}
 
 	@Test
 	void updateFrequencyTest1Hz() throws InterruptedException {
-		Files.init("Resources", RootFileLocation.LOCAL);
-		Log.init(Files.getCommonFolder(CommonFolders.Logs), true);
 		Input.init(1.000f);
 		Thread.sleep(5000);
 		float deltaTime = Input.stop();
-		Log.disposeAll();
-		assertEquals(1.000f, 1.000f / deltaTime, 0.05f);
+		assertEquals(1.000f, 1.000f / deltaTime, 0.1f);
+		Thread.sleep(5000);
 	}
 	
 	@Test
 	void updateFrequencyTest10Hz() throws InterruptedException {
-		Files.init("Resources", RootFileLocation.LOCAL);
-		Log.init(Files.getCommonFolder(CommonFolders.Logs), true);
 		Input.init(10.000f);
 		Thread.sleep(5000);
 		float deltaTime = Input.stop();
-		Log.disposeAll();
-		assertEquals(10.000f, 1.000f / deltaTime, 0.05f);
+		assertEquals(10.000f, 1.000f / deltaTime, 1.0f);
+		Thread.sleep(5000);
 	}
 	
 	@Test
 	void updateFrequencyTest75Hz() throws InterruptedException {
-		Files.init("Resources", RootFileLocation.LOCAL);
-		Log.init(Files.getCommonFolder(CommonFolders.Logs), true);
 		Input.init(75.000f);
 		Thread.sleep(5000);
 		float deltaTime = Input.stop();
-		Log.disposeAll();
-		assertEquals(75.000f, 1.000f / deltaTime, 0.05f);
+		assertEquals(75.000f, 1.000f / deltaTime, 7.5f);
+		Thread.sleep(5000);
 	}
 	
 	@Test
 	void updateFrequencyTest135Hz() throws InterruptedException {
-		Files.init("Resources", RootFileLocation.LOCAL);
-		Log.init(Files.getCommonFolder(CommonFolders.Logs), true);
 		Input.init(135.000f);
 		Thread.sleep(5000);
 		float deltaTime = Input.stop();
-		Log.disposeAll();
-		assertEquals(135.000f, 1.000f / deltaTime, 0.05f);
+		assertEquals(135.000f, 1.000f / deltaTime, 13.5f);
+		Thread.sleep(5000);
 	}
 
 }
