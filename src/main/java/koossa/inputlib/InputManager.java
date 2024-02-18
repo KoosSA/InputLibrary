85af15b93fbb9a6eeb040b52f332147c98981cac
Koos <kkposbus@gmail.com>
2024-02-18 23:32:33 +0200

    Added ability to check if key/nutton is pressed or down

--- src/main/java/koossa/inputlib/InputManager.java
+++ src/main/java/koossa/inputlib/InputManager.java
@@ -1,12 +1,21 @@
 package koossa.inputlib;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class InputManager {
 	
 	private InputData data;
+	private List<Integer> keysJustPressed;
+	private List<Integer> keysDown;
+	private List<Integer> mouseJustPressed;
+	private List<Integer> mouseDown;
 	
 	public InputManager() {
 		data = new InputData();
-		data.loadOverride();
+		data = InputData.loadOverride();
+		keysJustPressed = new ArrayList<Integer>();
+		keysDown = new ArrayList<Integer>();
 	}
 	
 	public InputData getData() {
@@ -19,6 +28,22 @@ class InputManager {
 
 	public void dispose() {
 		data.saveOverride();
+	}
+	
+	public boolean isFunctionKeyJustPressed(String function) {
+		return keysJustPressed.contains(data.getKeyBindings().get(function));
+	}
+	
+	public boolean isFunctionKeyDown(String function) {
+		return keysDown.contains(data.getKeyBindings().get(function));
+	}
+	
+	public boolean isFunctionMouseButtonJustPressed(String function) {
+		return mouseJustPressed.contains(data.getMouseBindings().get(function));
+	}
+	
+	public boolean isFunctionMouseButtonDown(String function) {
+		return mouseDown.contains(data.getMouseBindings().get(function));
 	}
 
 }
